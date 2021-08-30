cef2c92eb2b8ec0d0b45d4249233287c37d4a35f
nakrugs <80053949+nakrugs@users.noreply.github.com>
2021-08-30 21:48:18 +0200

    Destroyed 2DroneAnim

---
+++ Assets/Scripts/DroneAnimationTriggerNoHMI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneAnimationTriggerNoHMI : MonoBehaviour
+{
+    public Animator b;
+
+    private void OnTriggerEnter(Collider col)
+    
+        {
+
+            if (col.tag == "Player")
+            {
+                Debug.Log("ENTERED");
+                b.SetTrigger("DroneLeavesNoHMI");
+                b.Play("DroneLeavesNoHMI");
+                Destroy(this);
+            }
+        }
+}
