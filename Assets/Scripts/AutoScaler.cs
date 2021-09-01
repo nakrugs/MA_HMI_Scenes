33b113d45d967beea42dc993773626eb315a00cd
nakrugs <80053949+nakrugs@users.noreply.github.com>
2021-09-01 13:09:42 +0200

    Update AutoScaler.cs

--- Assets/Scripts/AutoScaler.cs
+++ Assets/Scripts/AutoScaler.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class AutoScaler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float defaultHeight = 1.8f;
+    [SerializeField]
+    private Camera camera;
+
+    private void Resize()
     {
-        
+        float headHeight = camera.transform.localPosition.y;
+        float scale = defaultHeight / headHeight;
+        transform.localScale = Vector3.one * scale;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        Resize();
     }
 }
